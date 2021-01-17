1b720a9bb7155e796c7abfef2ad812c9f2b12fd7
Antoni <antony_grone12@live.com>
2021-01-17 16:31:29 -0500

    slider noticias

--- public/js/script.js
+++ public/js/script.js
@@ -21,27 +21,6 @@ $(document).ready(function(){
             }
         ]
     });
-    $('.slideshow-section-portal').slick({
-        slidesToShow:3,
-        slideToScroll:1,
-        appendArrows: $(this).find('.slideshow-cart-control'),
-        prevArrow: '<button class="prev">&#10094;</button>',
-        nextArrow: '<button class="next">&#10095;</button>',
-        responsive:[
-            {
-                breakpoint:768,
-                settings:{
-                    slidesToShow:2
-                }
-            },
-            {
-                breakpoint:600,
-                settings:{
-                    slidesToShow:1
-                }
-            }
-        ]
-    });
 });
 
 
--- src/components/Portal.js
+++ src/components/Portal.js
@@ -2,6 +2,10 @@ import React, { useContext, useEffect } from 'react'
 import {url} from '../config/host'
 import NewContext from '../context/new/newContext';
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+
 function Portal() {
     const newContext = useContext(NewContext);
     const {news,getNews} = newContext;
@@ -19,12 +23,34 @@ function Portal() {
                 <h2>Noticias</h2>
                 <a href="!#">Ver todas las noticias <i className="fas fa-plus-circle"></i></a>
             </div>
-            <div className="slideshow slideshow-section-portal">
+            <Swiper
+                slidesPerView={3}
+                breakpoints={{
+                    1200:{
+                        slidesPerView:4
+                    },
+                    768:{
+                        slidesPerView:3
+                    },
+                    600:{
+                        slidesPerView:2
+                    },
+                    200:{
+                        slidesPerView:1
+                    }
+                }}
+                pagination
+                autoplay
+                effect="fade"
+                id="main"
+                className="slideshow slideshow-section-portal"
+                tag="div"
+            >
                 {
                     news.map(notice => {
                         if(notice.estado === 1) {
                             return (
-                                <div key={notice._id} className="slideshow-cart" style={{backgroundImage:`url(${url}/api/Noticia/${notice.portada})`}}>
+                                <SwiperSlide zoom={true} tag="div" key={notice._id} className="slideshow-cart" style={{backgroundImage:`url(${url}/api/Noticia/${notice.portada})`}}>
                                     <div className="icon">
                                         <p>{notice.subtitulo}</p>
                                     </div>
@@ -32,13 +58,13 @@ function Portal() {
                                         <h3>{notice.titulo}</h3>
                                         <div className="text-divider"></div>
                                     </div>
-                                </div>
+                                </SwiperSlide>
                             )
                         }
                         return null;
                     })
                 }
-            </div>
+            </Swiper>
             <div className="slideshow-cart-control">
             </div>
         </section>
